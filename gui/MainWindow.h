dde24a2f29bddd9a556fa1edbc0a7b76f840d098
Stuarth Ramirez <167147559+Stuarth195@users.noreply.github.com>
2025-09-14 20:41:56 -0600

    Interfas Grafica visualmete lista para implementar sockets y logica de grafiacas

--- gui/MainWindow.h
+++ gui/MainWindow.h
@@ -1,9 +1,64 @@
-#pragma once
-#include <QMainWindow>
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
-class MainWindow : public QMainWindow {
+#include <QMainWindow>
+#include <QtCharts/QChartView>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QLabel>
+#include <QTableWidget>
+#include <QSplitter>
+#include <QTabWidget>
+
+//QT_CHARTS_USE_NAMESPACE
+
+    class MainWindow : public QMainWindow
+{
     Q_OBJECT
+
 public:
-    explicit MainWindow(QWidget* parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    void setupOverviewTab();
+    void setupMemoryMapTab();
+    void setupAllocationByFileTab();
+    void setupMemoryLeaksTab();
+
+    // Overview Tab
+    QTabWidget *tabWidget;
+    QWidget *overviewTab;
+    QGridLayout *overviewLayout;
+    QLabel *currentMemoryLabel;
+    QLabel *activeAllocationsLabel;
+    QLabel *memoryLeaksLabel;
+    QLabel *maxMemoryLabel;
+    QLabel *totalAllocationsLabel;
+    QChartView *timelineChartView;
+    QTableWidget *topFilesTable;
+
+    // Memory Map Tab
+    QWidget *memoryMapTab;
+    QGridLayout *memoryMapLayout;
+    QTableWidget *memoryMapTable;
+
+    // Allocation by File Tab
+    QWidget *allocationByFileTab;
+    QGridLayout *allocationByFileLayout;
+    QChartView *allocationChartView;
+    QTableWidget *allocationTable;
+
+    // Memory Leaks Tab
+    QWidget *memoryLeaksTab;
+    QGridLayout *memoryLeaksLayout;
+    QLabel *totalLeakedMemoryLabel;
+    QLabel *biggestLeakLabel;
+    QLabel *mostFrequentLeakFileLabel;
+    QLabel *leakRateLabel;
+    QChartView *leaksByFileChartView;
+    QChartView *leaksDistributionChartView;
+    QChartView *leaksTimelineChartView;
 };
+
+#endif // MAINWINDOW_H
